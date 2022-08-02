1dcc1ccbca61369b6369a4cc1acacfd48b401d59
Jump T. Wong <awong1900@163.com>
2022-08-02 20:36:45 +0800

    feat: 使用ossutil

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,7 +12,7 @@ module.exports = {
     "/": {
       lang: "zh-CN",
       title: "一起踢足球训练营",
-      description: "非营利的足球团队训练，享受训练的乐趣。",
+      description: "包含非营利的足球团队训练，享受训练的乐趣。",
     },
     "/en/": {
       lang: "en-US",
