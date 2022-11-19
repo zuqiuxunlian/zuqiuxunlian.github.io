c7d4c21a1743721d06a1f601d9766ae099b95dd3
Jump T. Wong <awong1900@163.com>
2022-11-19 16:36:50 +0800

    feat:

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -40,12 +40,17 @@ module.exports = {
         lastUpdated: "最后更新",
         nav: [
           { text: "加入我们", link: "/get-started" },
+          { text: "招聘", link: "/hire" },
           {
             text: "公众号",
             link: "/wechat-official-accounts",
           },
           { text: "IORI计时器", link: "https://zuqiuxunlian.com/iori" },
           { text: "B站", link: "https://space.bilibili.com/388955673" },
+          {
+            text: "抖音",
+            link: "https://www.douyin.com/user/MS4wLjABAAAArUwEHeE_GuHNPJ3yyMRLYizeSS-8cGoaPWBI97E9L1w",
+          },
           // { text: "旧版本", link: "https://old.zuqiuxunlian.com" },
         ],
       },
